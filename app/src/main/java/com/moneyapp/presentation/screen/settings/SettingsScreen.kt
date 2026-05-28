2cfd321f867fad9629bf8c5a3177fc53ad0bb145
Jufry Siregar <mjuffrisiregar@gmail.com>
2026-05-28 12:06:18 +0700

    Updte setiing

--- app/src/main/java/com/moneyapp/presentation/screen/settings/SettingsScreen.kt
+++ app/src/main/java/com/moneyapp/presentation/screen/settings/SettingsScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -292,7 +293,7 @@ fun SettingsScreen(
 }
 
 @Composable
-private fun SettingsCard(content: @Composable Column.() -> Unit) {
+private fun SettingsCard(content: @Composable ColumnScope.() -> Unit) {
     Card(
         shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface),
